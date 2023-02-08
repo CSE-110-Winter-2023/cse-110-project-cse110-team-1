e84cf8e825438c29dc0053a6ee8b519f92e864ae
Anh Le <kunquan2345@gmail.com>
2023-02-08 12:09:30 -0800

    Create somewhat usable layout

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onAddLocationClick(View view) {
+        ScrollView parentLayout = findViewById(R.id.scroll_view);
+
+        
+
 
     }
 
