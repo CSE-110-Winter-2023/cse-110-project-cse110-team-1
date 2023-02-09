33b5342f4451f84968c8cd705d917276c8d4917f
astoriama <yim016@ucsd.edu>
2023-02-08 17:25:22 -0800

    added compass_activty

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void OnLaunchCompass(View view) {
+        Intent intent = new Intent(this, compass_activity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/socialcompass/compass_activity.java
@@ -0,0 +1,14 @@
+package com.example.socialcompass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class compass_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+    }
+}
