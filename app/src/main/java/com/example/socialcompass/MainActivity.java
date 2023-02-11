f720a51596942503b3b4a096b0e36fbdb7eaab8e
Anh Le <kunquan2345@gmail.com>
2023-02-11 14:23:42 -0800

    Add comment TODO

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -17,12 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
+        * TODO: keep track of what view should be displayed/hide
+        *  (and) display data that we already have on those fields
+        * This can be determine by checking which data we have available
+        * */
 
     }
 
     public void onAddLocationClick(View view) {
+        /*
+        * TODO: display additional form for user to fill in
+        * Each form has 3 input fields: label, longitude and latitude
+        * Store these data in storage
+        *  */
         ScrollView parentLayout = findViewById(R.id.scroll_view);
+    }
+
+    public void onSubmitClick(View view){
+        /*
+        * TODO: submit user's input and store them to storage
+        *  */
     }
 
 
