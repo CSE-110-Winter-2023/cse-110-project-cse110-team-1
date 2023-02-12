c9d9d856a17270b581b63c16a1fe982d1300f5ae
astoriama <yim016@ucsd.edu>
2023-02-11 22:38:07 -0800

    added algorithm, compass node label, adjusted node location

--- app/src/main/java/com/example/socialcompass/compass_activity.java
+++ app/src/main/java/com/example/socialcompass/compass_activity.java
@@ -1,8 +1,12 @@
 package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.util.Pair;
+import android.widget.ImageView;
 
 public class compass_activity extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class compass_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+        setAngle();
     }
+
+
+
+    float getAngle(float gpsLat, float gpsLong,float addressLat, float addressLong){
+        double len_long = gpsLong - addressLong;
+        double len_lat =  gpsLat - addressLat;
+        double angle_rad =  Math.atan2(len_lat,len_long);
+
+        double angle_deg =  angle_rad*180.0/Math.PI;
+        float degree = (float) angle_deg;
+
+        return degree;
+    }
+
+    void setAngle(){
+        // if there is only one location,
+        float gpsLat = (float) 32.8511987628294;
+        float gpsLong = (float) -117.23676356032297;
+        float addressLat = (float) 32.859057060490336;
+        float addressLong = (float) -117.24266799416866;
+        float angle1 = getAngle( gpsLat,  gpsLong, addressLat,  addressLong);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.compass_layout);
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+        constraintSet.constrainCircle(R.id.node_1, R.id.compass_img, 462, angle1);
+        constraintSet.constrainCircle(R.id.label_1, R.id.compass_img, 440, angle1);
+        constraintSet.applyTo(constraintLayout);
+
+    }
+
+
+
+
+
+
+
+
 }
