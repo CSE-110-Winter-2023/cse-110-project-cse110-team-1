cf0fbae4f75c0d5df5e304fd195812bffbc10c1f
GfromMacBook <gwuhou@ucsd.edu>
2023-02-11 23:50:06 -0800

    update algorithm

--- app/src/main/java/com/example/socialcompass/compass_activity.java
+++ app/src/main/java/com/example/socialcompass/compass_activity.java
@@ -24,29 +24,49 @@ public class compass_activity extends AppCompatActivity {
 
 
     float getAngle(float gpsLat, float gpsLong,float addressLat, float addressLong){
-        double len_long = gpsLong - addressLong;
-        double len_lat =  gpsLat - addressLat;
-        double angle_rad =  Math.atan2(len_lat,len_long);
+        float lenA = Math.abs(addressLong-gpsLong);
+        float lenB = Math.abs(addressLat - gpsLat);
+
+        double angle_rad =  Math.atan2(lenB,lenA);
 
         double angle_deg =  angle_rad*180.0/Math.PI;
         float degree = (float) angle_deg;
+
+        if(gpsLat<=addressLat && gpsLong<=addressLong){
+            // first quadrant
+            degree = 90-degree;
+        }else if(gpsLat<=addressLat && gpsLong>=addressLong){
+            // second quadrant
+            degree = degree+270;
+        }else if(gpsLat>=addressLat && gpsLong>=addressLong){
+            // third quadrant
+            degree = 270-degree;
+        }else if(gpsLat>=addressLat && gpsLong<=addressLong){
+            // forth quadrant
+            degree = degree+90;
+        }
 
         return degree;
     }
 
     void setAngle(){
         // if there is only one location,
-        float gpsLat = (float) 32.8511987628294;
-        float gpsLong = (float) -117.23676356032297;
-        float addressLat = (float) 32.859057060490336;
-        float addressLong = (float) -117.24266799416866;
-        float angle1 = getAngle( 1,  1, 2,  2);
+        float gpsLat = (float) 32.88074495280559;
+        float gpsLong = (float) -117.23403456410483;
+        float addressLat = (float) 32.87774383077887;
+        float addressLong = (float)  -117.23034561391084;
+        float angle1 = getAngle( gpsLat,  gpsLong, addressLat,  addressLong);
 
         ConstraintLayout constraintLayout = findViewById(R.id.compass_layout);
         ConstraintSet constraintSet = new ConstraintSet();
         constraintSet.clone(constraintLayout);
         constraintSet.constrainCircle(R.id.node_1, R.id.compass_img, 462, angle1);
         constraintSet.constrainCircle(R.id.label_1, R.id.compass_img, 330, angle1);
+
+
+        float angle2 = getAngle( gpsLat,  gpsLong, (float) 32.87550191021879,  (float) -117.2329838492025);
+        constraintSet.constrainCircle(R.id.node_2, R.id.compass_img, 462, angle2);
+
         constraintSet.applyTo(constraintLayout);
 
     }
