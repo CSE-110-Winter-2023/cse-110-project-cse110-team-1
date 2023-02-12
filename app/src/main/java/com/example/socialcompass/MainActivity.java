9bbb1ddf108f7c4d9c8e484b843e3fb76ab80d21
incisors <haj003@ucsd.edu>
2023-02-12 00:55:36 -0800

    Fixed the logistic of save data when click save, fix minor bugs

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -29,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
         View groupView_1 = findViewById(R.id.input_group_1);
         View groupView_2 = findViewById(R.id.input_group_2);
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        String label_0 = preferences.getString("label_0","default");
-        String longitude_0 = preferences.getString("longitude_0","default");
-        String latitude_0 = preferences.getString("latitude_0","default");
+        String label_0 = preferences.getString("label_0","Label");
+        String longitude_0 = preferences.getString("longitude_0","Longitude");
+        String latitude_0 = preferences.getString("latitude_0","Latitude");
 
         TextView labelView_0 = findViewById(R.id.label_0);
         labelView_0.setText(label_0);
@@ -40,14 +40,14 @@ public class MainActivity extends AppCompatActivity {
         TextView latitudeView_0 = findViewById(R.id.latitude_0);
         latitudeView_0.setText(latitude_0);
 
-        String label_1 = preferences.getString("label_1","default");
-        String longitude_1 = preferences.getString("longitude_1","default");
-        String latitude_1 = preferences.getString("latitude_1","default");
-        String label_2 = preferences.getString("label_2","default");
-        String longitude_2 = preferences.getString("longitude_2","default");
-        String latitude_2 = preferences.getString("latitude_2","default");
+        String label_1 = preferences.getString("label_1","Label");
+        String longitude_1 = preferences.getString("longitude_1","Longitude");
+        String latitude_1 = preferences.getString("latitude_1","Latitude");
+        String label_2 = preferences.getString("label_2","Label");
+        String longitude_2 = preferences.getString("longitude_2","Longitude");
+        String latitude_2 = preferences.getString("latitude_2","Latitude");
 
-        if (!label_1.equals("default")) {
+        if (!label_1.equals("Label")) {
             groupView_1.setVisibility(View.VISIBLE);
 
             TextView labelView_1 = findViewById(R.id.label_1);
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             TextView latitudeView_1 = findViewById(R.id.latitude_1);
             latitudeView_1.setText(latitude_1);
         }
-        if (!label_2.equals("default")) {
+        if (!label_2.equals("Label")) {
             groupView_2.setVisibility(View.VISIBLE);
 
             TextView labelView_2 = findViewById(R.id.label_2);
@@ -93,7 +93,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSubmitButtonClick(View view) {
+        View groupView_1 = findViewById(R.id.input_group_1);
+        View groupView_2 = findViewById(R.id.input_group_2);
 
+        //Get all label's text store in strings
         TextView labelView_0 = findViewById(R.id.label_0);
         String labelStr_0 = labelView_0.getText().toString().trim();
         TextView longitudeView_0 = findViewById(R.id.longitude_0);
@@ -101,83 +104,48 @@ public class MainActivity extends AppCompatActivity {
         TextView latitudeView_0 = findViewById(R.id.latitude_0);
         String latitudeStr_0 = latitudeView_0.getText().toString().trim();
 
+        TextView labelView_1 = findViewById(R.id.label_1);
+        String labelStr_1 = labelView_1.getText().toString().trim();
+        TextView longitudeView_1 = findViewById(R.id.longitude_1);
+        String longitudeStr_1 = longitudeView_1.getText().toString().trim();
+        TextView latitudeView_1 = findViewById(R.id.latitude_1);
+        String latitudeStr_1 = latitudeView_1.getText().toString().trim();
 
-        if (Utilities.isValidAll(labelStr_0,longitudeStr_0,latitudeStr_0)) {
+        TextView labelView_2 = findViewById(R.id.label_2);
+        String labelStr_2 = labelView_2.getText().toString().trim();
+        TextView longitudeView_2 = findViewById(R.id.longitude_2);
+        String longitudeStr_2 = longitudeView_2.getText().toString().trim();
+        TextView latitudeView_2 = findViewById(R.id.latitude_2);
+        String latitudeStr_2 = latitudeView_2.getText().toString().trim();
+
+        //determine all three group of fields are available and store together
+        if (Utilities.isValidAll(labelStr_0,longitudeStr_0,latitudeStr_0)
+                && Utilities.isValidAll(labelStr_1,longitudeStr_1,latitudeStr_1)
+                && Utilities.isValidAll(labelStr_2,longitudeStr_2,latitudeStr_2)) {
 
             SharedPreferences preferences = getPreferences(MODE_PRIVATE);
             SharedPreferences.Editor editor = preferences.edit();
+
             editor.putString("label_0",labelStr_0);
             editor.putString("longitude_0",longitudeStr_0);
             editor.putString("latitude_0",latitudeStr_0);
-            editor.apply();
 
-            String label = preferences.getString("label_0","default");
-            String longitude = preferences.getString("longitude_0","default");
-            String latitude = preferences.getString("latitude_0","default");
+            editor.putString("label_1",labelStr_1);
+            editor.putString("longitude_1",longitudeStr_1);
+            editor.putString("latitude_1",latitudeStr_1);
+
+            editor.putString("label_2",labelStr_2);
+            editor.putString("longitude_2",longitudeStr_2);
+            editor.putString("latitude_2",latitudeStr_2);
+            editor.apply();
+            Utilities.showAlert(this,"data Saved");
+
+            //Intent intent = new Intent(this, CompassActivity.class);
+            //link to new page
         }
         else {
-            Utilities.showAlert(this,"Input 1 is not Valid, Please check");
+            Utilities.showAlert(this,
+                    "Input is not Valid, Data not saved");
         }
-
-        View groupView_1 = findViewById(R.id.input_group_1);
-        View groupView_2 = findViewById(R.id.input_group_2);
-
-        if (groupView_1.getVisibility() == View.VISIBLE) {
-            TextView labelView_1 = findViewById(R.id.label_1);
-            String labelStr_1 = labelView_1.getText().toString().trim();
-            TextView longitudeView_1 = findViewById(R.id.longitude_1);
-            String longitudeStr_1 = longitudeView_1.getText().toString().trim();
-            TextView latitudeView_1 = findViewById(R.id.latitude_1);
-            String latitudeStr_1 = latitudeView_1.getText().toString().trim();
-            if (Utilities.isValidAll(labelStr_1,longitudeStr_1,latitudeStr_1)) {
-                SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("label_1",labelStr_1);
-                editor.putString("longitude_1",longitudeStr_1);
-                editor.putString("latitude_1",latitudeStr_1);
-                editor.apply();
-            }
-            else {
-                Utilities.showAlert(this,"Input 2 is not Valid, Please check");
-            }
-        }
-
-
-        if (groupView_2.getVisibility() == View.VISIBLE) {
-            TextView labelView_2 = findViewById(R.id.label_2);
-            String labelStr_2 = labelView_0.getText().toString().trim();
-            TextView longitudeView_2 = findViewById(R.id.longitude_2);
-            String longitudeStr_2 = longitudeView_0.getText().toString().trim();
-            TextView latitudeView_2 = findViewById(R.id.latitude_2);
-            String latitudeStr_2 = latitudeView_2.getText().toString().trim();
-            if (Utilities.isValidAll(labelStr_2,longitudeStr_2,latitudeStr_2)) {
-                SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("label_2",labelStr_2);
-                editor.putString("longitude_2",longitudeStr_2);
-                editor.putString("latitude_2",latitudeStr_2);
-                editor.apply();
-            }
-            else {
-                Utilities.showAlert(this,"Input 3 is not Valid, Please check");
-            }
-        }
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        String label_0 = preferences.getString("label_0","default");
-        String longitude_0 = preferences.getString("longitude_0","default");
-        String latitude_0 = preferences.getString("latitude_0","default");
-        String label_1 = preferences.getString("label_1","default");
-        String longitude_1 = preferences.getString("longitude_1","default");
-        String latitude_1 = preferences.getString("latitude_1","default");
-        String label_2 = preferences.getString("label_2","default");
-        String longitude_2 = preferences.getString("longitude_2","default");
-        String latitude_2 = preferences.getString("latitude_2","default");
-        Utilities.showAlert(this,label_0+" "+longitude_0+" "+latitude_0+ "\n"
-            +label_1+" "+longitude_1+" "+latitude_1+ "\n"
-                +label_2+" "+longitude_2+" "+latitude_2);
-
-        //Intent intent = new Intent(this, CompassActivity.class);
-        //link to new page
     }
-
 }
