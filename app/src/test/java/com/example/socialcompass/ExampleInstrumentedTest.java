aaad530f579e3e743d65d5df7af402a5034479bd
Anh Le <kunquan2345@gmail.com>
2023-02-19 18:22:24 -0800

    Fix the failing test

--- app/src/test/java/com/example/socialcompass/ExampleInstrumentedTest.java
+++ app/src/test/java/com/example/socialcompass/ExampleInstrumentedTest.java
@@ -23,6 +23,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("org.robolectric.default", appContext.getPackageName());
+        assertEquals("com.example.socialcompass", appContext.getPackageName());
     }
 }
