dc714b46110e4cfae3507f5ab6a20f2f6352612b
incisors <haj003@ucsd.edu>
2023-03-04 19:30:32 -0800

    implement the distance calculator function and test

--- app/src/main/java/com/example/socialcompass/Utilities.java
+++ app/src/main/java/com/example/socialcompass/Utilities.java
@@ -143,4 +143,17 @@ public class Utilities {
             return false;
         }
     }
+
+    public static double calculateDistanceInMiles(double lat1, double lon1, double lat2, double lon2) {
+        double earthRadiusInMiles = 3958.8; // radius of the earth in miles
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distanceInMiles = earthRadiusInMiles * c;
+        return distanceInMiles;
+    }
+
 }
