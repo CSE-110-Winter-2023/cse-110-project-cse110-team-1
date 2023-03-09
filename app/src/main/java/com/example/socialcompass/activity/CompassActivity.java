425b040f31cbfd343385a6dceaf5bf41f2112b7f
aleksaradomirovic <braleksa2@gmail.com>
2023-03-08 22:32:48 -0800

    finished compasslistener

--- app/src/main/java/com/example/socialcompass/activity/CompassActivity.java
+++ app/src/main/java/com/example/socialcompass/activity/CompassActivity.java
@@ -5,12 +5,13 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.lifecycle.LiveData;
 
-import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -22,6 +23,7 @@ import com.example.socialcompass.old.GPSLocationHandler;
 import com.example.socialcompass.old.OrientationService;
 import com.example.socialcompass.utility.Utilities;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class CompassActivity extends AppCompatActivity {
@@ -34,9 +36,9 @@ public class CompassActivity extends AppCompatActivity {
 
         // TODO synchronize against the activity drawing?
         ConstraintLayout layout = findViewById(R.id.compass_layout);
-//        layout.removeAllViews();
-        ConstraintSet cs = new ConstraintSet();
-        cs.clone(layout);
+        layout.removeAllViews();
+
+        ImageView compassimg = findViewById(R.id.compass_img);
 
         Log.d("CompassView","Redraw "+friendsList.getValue());
 
@@ -45,17 +47,33 @@ public class CompassActivity extends AppCompatActivity {
         final float gpsLat = loc.first.floatValue(),
                     gpsLon = loc.second.floatValue();
 
-        for(Friend f : friendsList.getValue()) {
+        List<ImageView> nodes = new LinkedList<>();
+        List<Friend> friends = friendsList.getValue();
+
+        for(Friend f : friends) {
             ImageView node = new ImageView(getApplicationContext());
             node.setImageIcon(nodeIcon);
-            node.setLayoutParams(new LinearLayout.LayoutParams(20,20));
             node.setId(View.generateViewId());
+            node.setLayoutParams(new LinearLayout.LayoutParams(50,50));
             layout.addView(node);
 
-            cs.constrainCircle(node.getId(), R.id.compass_img, 462, Utilities.getAngle(gpsLat, gpsLon, f.latitude, f.longitude));
+            node.setVisibility(View.VISIBLE);
+
+            nodes.add(node);
+        }
+
+        ConstraintSet cs = new ConstraintSet();
+        cs.clone(layout);
+
+        for(int i = 0; i < nodes.size(); i++) {
+            Friend f = friends.get(i);
+            Log.d("Node: ", ""+nodes.get(i).getId());
+            cs.constrainCircle(nodes.get(i).getId(), R.id.compass_layout, 480, Utilities.getAngle(gpsLat, gpsLon, f.latitude, f.longitude));
         }
 
         cs.applyTo(layout);
+
+
 
 //        Log.d("Layout", String.valueOf(layout.getChildCount()));
     }
@@ -84,4 +102,10 @@ public class CompassActivity extends AppCompatActivity {
         orientationService.getOrientation().observe(this, (a) -> { this.redrawAllFriends(); });
         locationService.getLocation().observe(this, (a) -> { this.redrawAllFriends(); });
     }
+
+    public void toFriendsList(View v) {
+        Intent intent = new Intent(this, FriendListActivity.class);
+        startActivity(intent);
+    }
+
 }
