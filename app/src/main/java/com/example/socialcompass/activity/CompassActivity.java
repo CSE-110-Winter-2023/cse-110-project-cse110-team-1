5ccbc9243be9deae850312a48e74d0576fcc2eac
astoriama <yim016@ucsd.edu>
2023-03-13 19:13:57 -0700

    zoom in zoom out

--- app/src/main/java/com/example/socialcompass/activity/CompassActivity.java
+++ app/src/main/java/com/example/socialcompass/activity/CompassActivity.java
@@ -1,14 +1,18 @@
 package com.example.socialcompass.activity;
 
+import static java.lang.String.valueOf;
+
 import android.content.Intent;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -30,6 +34,7 @@ import java.util.List;
 
 public class CompassActivity extends AppCompatActivity {
     private Icon nodeIcon;
+    private int displayCompass;
 
     public synchronized void redrawAllFriends() {
         if(locationService.getLocation().getValue() == null
@@ -75,12 +80,28 @@ public class CompassActivity extends AppCompatActivity {
         for(i = 0; i < friends.size(); i++) {
             Friend f = friends.get(i);
             float angle = Utilities.getAngle(gpsLat, gpsLon, f.latitude, f.longitude);
-            cs.constrainCircle(nodes.get(i).getId(), R.id.compass_layout, 462, angle);
+
             labels.get(i).setText( String.format("%s\n%.0fmi",f.label, Utilities.calculateDistanceInMiles(gpsLat, gpsLon, f.latitude, f.longitude)));
-            cs.constrainCircle(labels.get(i).getId(), R.id.compass_layout, 330, angle);
+            double dist_in_mile = Utilities.calculateDistanceInMiles(gpsLat, gpsLon, f.latitude, f.longitude);
+            int dist_in_scale = Utilities.calculateRadius(displayCompass,dist_in_mile);
+
+
+//            cs.constrainCircle(labels.get(i).getId(), R.id.compass_layout, (int) dist_in_scale, angle);
+            cs.constrainCircle(nodes.get(i).getId(), R.id.compass_layout, dist_in_scale, angle);
+//            cs.constrainCircle(nodes.get(i).getId(), R.id.compass_layout, 462, angle);
+            cs.constrainCircle(labels.get(i).getId(), R.id.compass_layout, dist_in_scale, angle);
 
             nodes.get(i).setVisibility(View.VISIBLE);
             labels.get(i).setVisibility(View.VISIBLE);
+
+//            if(dist_in_scale==462){
+//                nodes.get(i).setVisibility(View.VISIBLE);
+//                labels.get(i).setVisibility(View.INVISIBLE);
+//            }else{
+//                nodes.get(i).setVisibility(View.INVISIBLE);
+//                labels.get(i).setVisibility(View.VISIBLE);
+//            }
+
         }
         for(; i < nodes.size(); i++) {
             nodes.get(i).setVisibility(View.INVISIBLE);
@@ -88,8 +109,6 @@ public class CompassActivity extends AppCompatActivity {
         }
 
         cs.applyTo(layout);
-
-
 
 //        Log.d("Layout", String.valueOf(layout.getChildCount()));
     }
@@ -117,6 +136,61 @@ public class CompassActivity extends AppCompatActivity {
 
         orientationService.getOrientation().observe(this, (a) -> { this.redrawAllFriends(); });
         locationService.getLocation().observe(this, (a) -> { this.redrawAllFriends(); });
+
+        // default start at compass view 2
+        displayCompass = 2;
+        visibleCompass(displayCompass);
+        Button zoomInButton = this.findViewById(R.id.zoom_in_button);
+        Button zoomOutButton = this.findViewById(R.id.zoom_out_button);
+
+        zoomOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(displayCompass<4){
+                    displayCompass = displayCompass+1;
+                    visibleCompass(displayCompass);
+                }else{
+                    Toast.makeText(CompassActivity.this,
+                            "Cannot zoom out further", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        zoomInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(displayCompass>1){
+                    displayCompass = displayCompass-1;
+                    visibleCompass(displayCompass);
+                }else{
+                    Toast.makeText(CompassActivity.this,
+                            "Cannot zoom in further", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+    /*
+    * Function to control only one compass view is displayed
+    * */
+    private void visibleCompass(int compass_no){
+        for(int i = 1; i<=4; i++){
+            if( i != compass_no){
+                String compass_id = "imageView"+ valueOf(i);
+                int id = getResources().getIdentifier(compass_id, "id", getPackageName());
+                ImageView asVisible = findViewById(id);
+                asVisible.setVisibility(View.INVISIBLE);
+            }else{
+                String compass_id = "imageView"+ valueOf(compass_no);
+                int id = getResources().getIdentifier(compass_id, "id", getPackageName());
+                ImageView asVisible = findViewById(id);
+                asVisible.setVisibility(View.VISIBLE);
+
+            }
+        }
     }
 
     public void toFriendsList(View v) {
