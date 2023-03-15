c4f85f46f8ddeb8f3a825eaa7f268b9d2fd47355
Anh Le <kunquan2345@gmail.com>
2023-03-15 04:05:23 -0700

    The test actually does not work
    - Before, I thought just observe the LiveData would solve the problem
      Turn our, it does not. The test just skip over the observer and does not execute the assertion at all
    - Some attemps have been made like  let the main thread sleep - fail badly
    - Let the main thread waits for all other background also fail because it seems like the test finishes running before
      the activity finish all its works. Only the background activity are done (in this case, API call), while other tasks
      only finished running after the test is done
    - Worse case, may attempt to try out Espresso

--- app/src/androidTest/java/com/example/socialcompass/FriendListActivityTest.java
+++ app/src/androidTest/java/com/example/socialcompass/FriendListActivityTest.java
@@ -1,39 +1,26 @@
 package com.example.socialcompass;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import android.content.Context;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
-
-import com.example.socialcompass.model.friend.Friend;
-import com.example.socialcompass.model.friend.FriendDao;
-import com.example.socialcompass.model.friend.FriendDatabase;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.socialcompass.activity.FriendListActivity;
-import com.example.socialcompass.viewmodel.FriendListViewModel;
+import com.example.socialcompass.model.friend.FriendDao;
+import com.example.socialcompass.model.friend.FriendDatabase;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 @RunWith(AndroidJUnit4.class)
 public class FriendListActivityTest {
     FriendDatabase testDb;
@@ -46,7 +33,6 @@ public class FriendListActivityTest {
                 .allowMainThreadQueries()
                 .build();
         FriendDatabase.inject(testDb);
-
         friendListItemDao = testDb.getDao();
     }
 
@@ -65,23 +51,33 @@ public class FriendListActivityTest {
 
             //the before database
             var beforeDao = friendListItemDao.getAll();
+
+
             newPublicCode.requestFocus();
             //enter a new public code
             newPublicCode.setText(newText);
             //click on ADD
             addTodoButton.performClick();
 
+            var afterFriend = friendListItemDao.getAllLive();
+            afterFriend.observe(activity,(a)->{});
+
             //the new Friend should be added to the database
-            var afterDao = friendListItemDao.getAllLive();
-
-            afterDao.observe(activity, (a) -> {
-                if (a.size() != beforeDao.size()) {
-                    assertEquals(beforeDao.size()+1,a.size());
+            CountDownLatch latch = new CountDownLatch(10);
+            try {
+                if (!latch.await(6, TimeUnit.SECONDS)) {
+                    // we manually stop all threads and resume activity
+                    latch.countDown();
+                    latch.countDown();
+                    latch.countDown();
                 }
-            });
+            } catch (InterruptedException e) {
+                throw new RuntimeException("something is extremely wrong with this");
+            }
+            newPublicCode.requestFocus();
+            String currentText = String.valueOf(newPublicCode.getText());
 
-//
-
+            Log.d("AFTER_COUNTDOWN", currentText);
         });
     }
 }
