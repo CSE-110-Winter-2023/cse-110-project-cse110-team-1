08dec861b64766dd7d6215cdbdba86cd402b4825
astoriama <yim016@ucsd.edu>
2023-03-16 00:40:42 -0700

    added zoom in zoom out test in developer story

--- app/src/androidTest/java/com/example/socialcompass/MS2DeveloperStoryTest.java
+++ app/src/androidTest/java/com/example/socialcompass/MS2DeveloperStoryTest.java
@@ -1,16 +1,20 @@
 package com.example.socialcompass;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.actionWithAssertions;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withTagValue;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.intent.Intents;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
@@ -32,6 +36,7 @@ import androidx.test.rule.GrantPermissionRule;
 import com.example.socialcompass.activity.FriendListActivity;
 import com.example.socialcompass.activity.UserActivity;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -76,13 +81,57 @@ public class MS2DeveloperStoryTest {
         onView(withId(R.id.my_name_from_friendlist)).check(matches(withText(testInputName)));
 
 
-        onView(withId(R.id.new_friend_public_code)).perform(typeText("313757"), closeSoftKeyboard());
+        onView(withId(R.id.new_friend_public_code)).perform(typeText("mydream"), closeSoftKeyboard());
+        Thread.sleep(1000);
+        onView(withId(R.id.add_friend_btn)).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.new_friend_public_code)).perform(typeText("337891"), closeSoftKeyboard());
+        Thread.sleep(1000);
+        onView(withId(R.id.add_friend_btn)).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.new_friend_public_code)).perform(typeText("273530"), closeSoftKeyboard());
         Thread.sleep(1000);
         onView(withId(R.id.add_friend_btn)).perform(click());
         Thread.sleep(1000);
         onView(withId(R.id.compass_view_button)).perform(click());
-        Thread.sleep(5000);
-        int numberOfFriends = 1;
+        Thread.sleep(2000);
+        onView(withId(R.id.imageView1)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView2)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.imageView3)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView4)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+
+
+        int numberOfFriends = 3;
+        for (int i = 1; i <= numberOfFriends; i++) {
+            ViewInteraction friendViewInteraction = onView(
+                    allOf(withId(R.id.compass_layout)));
+            friendViewInteraction.check(matches(isDisplayed()));
+            onView(withTagValue(is((Object) "node_0"))).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+            onView(withTagValue(is((Object) "node_0"))).check(matches(isDisplayed()));
+        }
+        onView(withId(R.id.zoom_in_button)).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.imageView1)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.imageView2)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView3)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView4)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+
+        for (int i = 1; i <= numberOfFriends; i++) {
+            ViewInteraction friendViewInteraction = onView(
+                    allOf(withId(R.id.compass_layout)));
+            friendViewInteraction.check(matches(isDisplayed()));
+            onView(withTagValue(is((Object) "node_0"))).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+            onView(withTagValue(is((Object) "node_0"))).check(matches(isDisplayed()));
+        }
+        onView(withId(R.id.zoom_out_button)).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.zoom_out_button)).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.imageView1)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView2)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView3)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.imageView4)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+
         for (int i = 1; i <= numberOfFriends; i++) {
             ViewInteraction friendViewInteraction = onView(
                     allOf(withId(R.id.compass_layout)));
@@ -91,5 +140,35 @@ public class MS2DeveloperStoryTest {
             onView(withTagValue(is((Object) "node_0"))).check(matches(isDisplayed()));
         }
 
+        onView(withId(R.id.zoom_out_button)).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.imageView1)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView2)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView3)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.imageView4)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+    }
+    private CharSequence getTextFromTextView(ViewInteraction viewInteraction) {
+        final CharSequence[] textHolder = new CharSequence[1];
+
+        viewInteraction.perform(actionWithAssertions(new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextView textView = (TextView) view;
+                textHolder[0] = textView.getText();
+            }
+        }));
+
+        return textHolder[0];
     }
 }
